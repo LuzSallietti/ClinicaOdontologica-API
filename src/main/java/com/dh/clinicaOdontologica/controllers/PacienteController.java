3981b9cd7984470bbe2d92ee7b92a96969d6e825
LuzSallietti <luzsallietti@gmail.com>
2022-11-16 19:43:44 -0300

    Response Entity: prueba exitosa agregada en Pacientes

--- src/main/java/com/dh/clinicaOdontologica/controllers/PacienteController.java
+++ src/main/java/com/dh/clinicaOdontologica/controllers/PacienteController.java
@@ -2,6 +2,8 @@ package com.dh.clinicaOdontologica.controllers;
 
 import com.dh.clinicaOdontologica.controllers.dominio.Paciente;
 import com.dh.clinicaOdontologica.services.PacienteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -37,4 +39,27 @@ public class PacienteController {
         return pacienteService.buscar(email);
     }
 
+    //endpoint-> practicar ResponseEntity
+    @GetMapping("/saludo")
+    ResponseEntity<String> saludo(){
+        return new ResponseEntity<>("Bienvenido paciente", HttpStatus.OK);
+    }
+
+    //endpoint->practicar ResponseEntity
+    @PostMapping("/registrar")
+    ResponseEntity<Paciente>registrar(@RequestBody Paciente paciente){
+        return new ResponseEntity<>(pacienteService.crearPaciente(paciente),HttpStatus.CREATED);
+    }
+
+    @PutMapping("/actualizar")
+    ResponseEntity<Paciente> actualizar(@RequestBody Paciente paciente){
+        ResponseEntity response = null;
+        if(pacienteService.buscar(paciente.getEmail())==null){
+            response = new ResponseEntity<>("Email no encontrado",HttpStatus.NOT_FOUND);
+        } else {
+            //acá el service ejecuta la actualizacion y devuelve el paciente modificado
+            response = new ResponseEntity<>("Actualizado correctamente",HttpStatus.OK);
+        }
+    return response;
+    }
 }
--- src/main/java/com/dh/clinicaOdontologica/services/PacienteService.java
+++ src/main/java/com/dh/clinicaOdontologica/services/PacienteService.java
@@ -38,4 +38,5 @@ public class PacienteService {
         return pacienteEncontrado;
 
     }
+
 }
